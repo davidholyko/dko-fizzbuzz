9ee50b7bcfde09af2fae9490f6a762a8f9ec0c3b
davidholyko <david.holy.ko@gmail.com>
2019-03-24 18:30:13 -0400

    add fizzbuzz sort

--- assets/scripts/fizzbuzz/fizzbuzz.js
+++ assets/scripts/fizzbuzz/fizzbuzz.js
@@ -2,7 +2,7 @@ const fizzbuzz = function (values, max) {
   console.log('fizzbuzz')
   for (let i = 1; i <= max; i++) {
     let output = ''
-    values.forEach((item, index) => { if (!(i % values[index].number)) { output += values[index].word } })
+    values.sort((a, b) => a.number - b.number).forEach((item, index) => { if (!(i % values[index].number)) { output += values[index].word } })
     output ? $('#fizzbuzz-output').append(`<h1>${i}: ${output}</h1>`) : $('#fizzbuzz-output').append(`<h1>${i}: ${i}</h1>`)
   }
 }
